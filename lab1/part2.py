61451cded94b6c967fd00bb4059ca9084c1e32cd
Lunessel <varloky@gmail.com>
2023-12-09 14:53:18 +0200

    solution of lab1

---
+++ lab1/part2.py
@@ -0,0 +1,18 @@
+movie = "Harry Potter"
+is_employee = False
+is_employer = True
+is_sunny = True
+is_raining = False
+
+print("movie: ", movie)
+print("and: ", is_employee and is_employer)
+print("or: ", is_employee or is_employer)
+print("not is_employee: ", not is_employee)
+print("not is_employer: ", not is_employer)
+
+print("not is_sunny: ", not is_sunny)
+print("not is_raining: ", not is_raining)
+
+
+
+
